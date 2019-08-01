2c3fafdba9ba18e5c5757619f470714167b2de55
Alexandre Beaudouin <alexandre.beaudouin@dekra.com>
2019-08-01 12:07:50 +0200

    add firebase

--- index.js
+++ index.js
@@ -1,28 +1,49 @@
-const express = require('express');
+const express = require("express");
+const admin = require("firebase-admin");
 const App = express();
-const LBC = require('leboncoin-api');
-const initLat = {start: 44.837491, end: 44.856632}
-const initLng = {start: -0.590386, end: -0.567292}
+const LBC = require("leboncoin-api");
+const data = require("./data.json");
+var credentials = require("./credentials.json");
+var collection = "annonces";
+// const initLat = { start: 44.837491, end: 44.856632 };
+// const initLng = { start: -0.590386, end: -0.567292 };
 
-function getData(){
-    var search = new LBC.Search()
-    .setPage(1)
-    .setFilter(LBC.FILTERS.PARTICULIER)
-    .setCategory("locations")
-    .setRegion("gironde")
-    .setLocation([
-                 {"zipcode": "33000"},
-                 {"zipcode": "33300"},
-                 ])
-                 
-    var results = search.run().then(function(data) {
-        return data.results.filter(f => f.location.lat >= initLat.start && f.location.lat <= initLat.end && f.location.lng >= initLng.start && f.location.lng <= initLng.end);
-    });
-    return results
+admin.initializeApp({
+  credential: admin.credential.cert(credentials),
+  databaseURL: "https://leboncoin-d7b55.firebaseio.com"
+});
+var firestore = admin.firestore();
+if (data && typeof data === "object") {
+  Object.keys(data).forEach(docKey => {
+    firestore
+      .collection(collection)
+      .doc(docKey)
+      .set(data[docKey])
+      .then(res => {
+        console.log("Document " + docKey + " successfully written!");
+      })
+      .catch(error => {
+        console.error("Error writing document: ", error);
+      });
+  });
 }
+// console.log(db.collection("annonces"));
+// function getData() {
+//   var search = new LBC.Search()
+//     .setPage(1)
+//     .setFilter(LBC.FILTERS.PARTICULIER)
+//     .setCategory("locations")
+//     .setRegion("gironde")
+//     .setLocation([{ zipcode: "33000" }, { zipcode: "33300" }]);
 
-App.get('/', async function(req, res){
-    console.log(await getData())
-})
+//   search.run().then(function(data) {
+//     console.log("data", data);
+//     return data;
+//   });
+// }
+// console.log(getData());
+// App.get('/', async function(req, res){
+//     console.log(await getData())
+// })
 
-App.listen(3000)
+// App.listen(4000)
