ca1a660c5b589ef21fe770898ce36e9b019014f8
Alexandre Beaudouin <alexandre.beaudouin@dekra.com>
2019-08-09 13:04:29 +0200

    refacto

--- index.js
+++ index.js
@@ -1,107 +1,97 @@
-const cron = require('node-cron');
-const file = require('./findFlat')
-
-
-
+const cron = require("node-cron");
 const LBC = require("leboncoin-api");
-const fileData = require('./data.json')
 const nodemailer = require("nodemailer");
-const credentials = require('./credentials.json')
-var fs = require('fs')
-let home = process.env['HOME']
-console.log(home)
+const credentials = require("./credentials.json");
+const fs = require("fs");
 
-
-cron.schedule('*/2 * * * *', () => {
-
-function writeToFIle(data){
-  const json = JSON.stringify(data, null, 2)
-  fs.writeFile('data.json', json, function(error){
-    if(error){
-      console.log(error)
+function writeToFIle(data) {
+  const json = JSON.stringify(data, null, 2);
+  fs.writeFile(`${__dirname}/data.json`, json, function(error) {
+    if (error) {
+      console.log(error);
     } else {
-      console.log('success')
+      console.log("success");
     }
-  })
-}
-
-function sendEmail(data){
-  var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-           user: credentials.EMAIL,
-           pass: credentials.PASSWORD
-       }
-   });
-  
-   const mailOptions = {
-    from: 'myserver@gmail.com', 
-    to: credentials.EMAIL,
-    subject: 'Dernière annonce LBC', 
-    html:   '<p>'+data.title+'</p><br/><p>'+data.date+'</p><br/><p>'+data.desc+'</p><br/><a href="'+data.link+'">'+data.link+'</a>'
-  };
-  
-  transporter.sendMail(mailOptions, function (err, info) {
-    if(err)
-      console.log(err)
-    else
-      console.log(res)
   });
 }
 
-function buildDataModel(data){
+function sendEmail(data) {
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: credentials.EMAIL,
+      pass: credentials.PASSWORD
+    }
+  });
+
+  const mailOptions = {
+    from: "myserver@gmail.com",
+    to: credentials.EMAIL,
+    subject: "Dernière annonce LBC",
+    html:
+      "<p>" +
+      data.title +
+      "</p><br/><p>" +
+      data.date +
+      "</p><br/><p>" +
+      data.desc +
+      '</p><br/><a href="' +
+      data.link +
+      '">' +
+      data.link +
+      "</a>"
+  };
+
+  transporter.sendMail(mailOptions, function(err, info) {
+    if (err) console.log(err);
+    else console.log(res);
+  });
+}
+
+function buildDataModel(data) {
   const annonces = {};
-  (data || []).map(el => {
-    const date = `${el.date.getDate()}/${el.date.getMonth()}/${el.date.getFullYear()}`
+  data.map(el => {
+    const date = `${el.date.getDate()}/${el.date.getMonth()}/${el.date.getFullYear()}`;
     annonces[el.id] = {
       id: el.id,
-      title: el.title,    
+      title: el.title,
       link: el.link,
       desc: el.description,
-      date
+      date: el.date
+    };
+  });
+  return annonces;
+}
+
+function sendDataToUser(data) {
+  const annonces = buildDataModel(data);
+  const annoncesLbcIds = Object.keys(annonces);
+  const fileData = JSON.parse(fs.readFileSync(`${__dirname}/data.json`));
+  const dataFileIds = Object.keys(fileData);
+  annoncesLbcIds.forEach(key => {
+    const include = dataFileIds.includes(key);
+    if (!include) {
+      console.log("not included", [annonces[key]]);
+      fileData[annonces[key].id] = annonces[key];
+      sendEmail(annonces[key]);
     }
   });
-      return annonces
-  }
+  writeToFIle(fileData);
+}
 
-  let notIncluded = []
+cron.schedule("*/2 * * * *", () => {
   var search = new LBC.Search()
-  .setPage(1)
-  .setFilter(LBC.FILTERS.ALL)
-  .setCategory("locations")
-  .setRegion("gironde")
-  .setLocation([{"zipcode": "33000"},{"zipcode": "33300"}])
-  .addSearchExtra("price", {min: 800, max: 1000}) 
-  .setSort({sort_by:"date",sort_order:"desc"})
-  
-  search.run().then( function(data) {
-    const annonces = data && buildDataModel(data.results);
-    // writeToFIle(annonces)
-    const dataFileIds = Object.keys(fileData)
-    const annoncesIds = Object.keys(annonces)
-    annoncesIds.forEach(key => {
-      const include = dataFileIds.includes(key)
-      if(!include) {
-        console.log('not included', [annonces[key]])
-        notIncluded.push({[annonces[key].id]: annonces[key]})
-        sendEmail(annonces[key])
-       }
-     });
+    .setPage(1)
+    .setFilter(LBC.FILTERS.ALL)
+    .setCategory("locations")
+    .setRegion("gironde")
+    .setLocation([{ zipcode: "33000" }, { zipcode: "33300" }])
+    .addSearchExtra("price", { min: 800, max: 1000 })
+    .setSort({ sort_by: "date", sort_order: "desc" });
 
-     if(notIncluded.length > 0) {
-      fs.readFile(`${home}/Documents/LBC/data.json`, function(err, data){
-        let obj = JSON.parse(data)
-        notIncluded.forEach(el => {
-          return obj = {...obj, ...el}
-         })
-         writeToFIle(obj)
-      })
-     }
-    });
-    console.log('running a task every 5 minutes');
+  search.run().then(function(data) {
+    data && sendDataToUser(data.results);
+  });
+
+  console.log("running a task every 2 minutes");
 });
-  
-
-
-
-
